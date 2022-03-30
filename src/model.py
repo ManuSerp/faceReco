2ea8db3c254840d84423955b40a8da9a8dc75197
Serp <manu.serpette74@gmail.com>
2022-03-30 09:28:31 +0200

    model

--- src/model.py
+++ src/model.py
@@ -6,6 +6,8 @@ import cv2
 from genDat import *
 backend.backend()
 backend.image_data_format()
+
+print("lib imported")
 # Le modèle initial en comprenant les # est meilleur mais trop lent
 model = Sequential([
 
@@ -22,12 +24,18 @@ model = Sequential([
     layers.Dense(2, activation='softmax')
 ])
 
+print("model setuped")
+
 model.compile(optimizer='adam',
               loss="binary_crossentropy",
               metrics=['acc'],)
 
+print("model compiled")
+
+
 nombre_epochs = 20
 
+print('training...')
 training = model.fit(genDat(), epochs=nombre_epochs)
 
 
@@ -40,24 +48,24 @@ plt.plot(xc, train_acc, xc, training.history['val_acc'], 'red')
 plt.show()
 
 
-img = preprocessing.image.load_img('voiture.jpg')
+# img = preprocessing.image.load_img('voiture.jpg')
 
-img = np.array(img) / 255.0
+# img = np.array(img) / 255.0
 
-img = cv2.resize(img, dsize=(224, 224))
-img = np.array(img).reshape(1, 224, 224, 3)
-predictions = (model.predict(img) > 0.5).astype("int32")
-print(predictions)
+# img = cv2.resize(img, dsize=(224, 224))
+# img = np.array(img).reshape(1, 224, 224, 3)
+# predictions = (model.predict(img) > 0.5).astype("int32")
+# print(predictions)
 
-img = preprocessing.image.load_img('avion.jfif')
+# img = preprocessing.image.load_img('avion.jfif')
 
-img = np.array(img) / 255.0
+# img = np.array(img) / 255.0
 
-img = cv2.resize(img, dsize=(224, 224))
-img = np.array(img).reshape(1, 224, 224, 3)
-predictions = (model.predict(img) > 0.5).astype("int32")
-print(predictions)
+# img = cv2.resize(img, dsize=(224, 224))
+# img = np.array(img).reshape(1, 224, 224, 3)
+# predictions = (model.predict(img) > 0.5).astype("int32")
+# print(predictions)
 
-scores = model.evaluate(test_generator, verbose=0)
+# scores = model.evaluate(test_generator, verbose=0)
 
-print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+# print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
